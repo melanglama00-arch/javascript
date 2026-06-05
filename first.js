8c5ad6830204050438791ba14309c3114564fec3
melang lama <melanglama00@gmail.com>
2026-06-05 11:30:38 +0545

    practising the operators of ja

--- first.js
+++ first.js
@@ -1,2 +1,18 @@
-console.log("hello nepal");
-console.log("hi ");
+
+//the meethod to print the messages in js
+console.log("\nhello nepal");
+console.log("\nhello milan  lama");
+console.log("\nthis is my learing area");
+
+//application of operators in js
+//simple example of calculator
+let a = 10;
+let b = 5;
+console.log("sum: ", a + b);
+console.log("sub= ",a - b);
+console.log("mul= ",a * b);
+console.log("modulus: ", a % b);
+console.log("division: ", a /b);
+//unary operator
+console.log(a++);
+console.log(b--);
