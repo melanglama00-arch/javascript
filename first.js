8f8f052f2973720c8fddf8549d9e85f2c9789613
melang lama <melanglama00@gmail.com>
2026-06-05 12:22:55 +0545

    practising the conditional statement

--- first.js
+++ first.js
@@ -9,10 +9,19 @@ console.log("\nthis is my learing area");
 let a = 10;
 let b = 5;
 console.log("sum: ", a + b);
-console.log("sub= ",a - b);
-console.log("mul= ",a * b);
+console.log("sub: ",a - b);
+console.log("mul: ",a * b);
 console.log("modulus: ", a % b);
 console.log("division: ", a /b);
 //unary operator
-console.log(a++);
-console.log(b--);
+console.log(a++);//increment
+console.log(b--);//decrement
+
+//small practise of js
+alert("welcome to the learning area");
+let num = prompt("enter a number");
+if( num % 5=== 0){
+    console.log(num,"is the multiple of 5");
+} else{
+    console.log(num,"not the multiple of 5");
+}
