f0f0de7a852cdb053e20da0b152d3a1b51b3c3ac
melang lama <melanglama00@gmail.com>
2026-06-06 09:32:03 +0545

    printing the array using the loop

--- second.js
+++ second.js
@@ -2,5 +2,18 @@ console.log("welcome to the second file");
 //practising the array
 let marks=[55,78,34,35,32,24];
 console.log(marks);
-let names=["milan", "melang", "milanjuk", "wangel"];
-console.log(names);
+
+//the looping over the arrays
+let names=["milan", "melang", "milanjuk", "wangel", "ram", "sita", "balram"];
+for(let idx=0; idx < names.length; idx++) {
+    console.log(names[idx]);
+}
+let arr=[34,56,78,43,33,24,44];
+let sum=0;
+for (let val of arr) {
+    sum+=val;
+}
+
+ let ave=sum/arr.length;
+    console.log(`the average of the given array numbers = ${ave}`);
+    console.log("this i show the learning happens");
