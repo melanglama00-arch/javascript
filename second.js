ba8c199230db1ecb4f8a3b1a1f8380f1c5b9dec5
melang lama <melanglama00@gmail.com>
2026-06-06 08:19:57 +0545

    creating new js file

---
+++ second.js
@@ -0,0 +1,6 @@
+console.log("welcome to the second file");
+//practising the array
+let marks=[55,78,34,35,32,24];
+console.log(marks);
+let names=["milan", "melang", "milanjuk", "wangel"];
+console.log(names);
