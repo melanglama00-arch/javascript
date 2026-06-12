a735673d8a3a4bf77ec348ff2d2cbfe661113e93
melang lama <melanglama00@gmail.com>
2026-06-12 13:54:10 +0545

    nested loop practising

--- calculator.js
+++ calculator.js
@@ -21,5 +21,38 @@ else if (operator == "/") {
     }
 }
 else {
-    console.log("you enter in valid operator.");
+    console.log("you enter invalid operator.");
+}
+
+// for loop in js
+for(var i=0; i<10; i++){
+    console.log(i);
+}
+//to print the even numbers upto 20
+console.log("  ")
+console.log("  ")
+
+let num=2
+for(num; num<=20; num++){
+    if(num%2==0){
+        console.log(num)
+    }
+
+}
+
+console.log("  ")
+var num5=5
+for(let i=1; i<10; i++){
+    console.log(i * num5 )
+}
+
+//nested loop in js
+for(let i=1; i<5; i++){
+    let pattern=" "
+    for(var j=i; J<=i; j++){
+        console.log("*")
+
+    }
+    console.log(pattern)
+
 }
---
+++ loop.js
@@ -0,0 +1,31 @@
+//this is the oractising mode for loop
+
+for(let i=1; i<=5; i++){
+    let p="";
+    for(let j=1; j<=i; j++){
+        p+="*";
+    }
+    console.log(p)
+}
+
+console.log("")
+console.log("")
+
+for(let i=5; i>=1; i--){
+    let p1 ="";
+    for(let j=1; j<=i; j++){
+        p1 +="*";
+    }
+    console.log(p1)
+}
+
+console.log("")
+console.log("")
+
+for(let i=1; i<=5; i++){
+    let p2 ="";
+    for(let j=1; j<=5; j++){
+        p2 +="*";
+    }
+    console.log(p2)
+}
